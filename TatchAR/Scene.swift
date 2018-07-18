c4bf80fb5da69d9015edf96bf0f984276fd646d5
Gabriel Wong <gabrieljw1@gmail.com>
2018-07-18 14:17:28 -0400

    Scoring Added
    Scoring support + made all sprites ! optional.

--- TatchAR/Scene.swift
+++ TatchAR/Scene.swift
@@ -12,21 +12,22 @@ import GameplayKit
 
 class Scene: SKScene, SKPhysicsContactDelegate {
     //Layer Nodes
-    private var backgroundNode: SKNode?
-    private var enemyNode: SKNode?
-    private var bulletNode: SKNode?
-    private var foregroundNode: SKNode?
+    private var backgroundNode: SKNode!
+    private var enemyNode: SKNode!
+    private var bulletNode: SKNode!
+    private var foregroundNode: SKNode!
     
     //Misc. Nodes
-    private var shooterLeft: SKSpriteNode?
-    private var crosshairs: SKSpriteNode?
-    private var shootButton: SKSpriteNode?
-    private var arrowLeft: SKSpriteNode?
-    private var arrowRight: SKSpriteNode?
+    private var shooterLeft: SKSpriteNode!
+    private var crosshairs: SKSpriteNode!
+    private var shootButton: SKSpriteNode!
+    private var arrowLeft: SKSpriteNode!
+    private var arrowRight: SKSpriteNode!
+    private var scoreLabel: SKLabelNode!
     
     //Groups
-    private var enemyAnchors: [ARAnchor]?
-    private var bulletAnchors: [ARAnchor]?
+    private var enemyAnchors: [ARAnchor] = []
+    private var bulletAnchors: [ARAnchor] = []
     
     //Display & Alerts
     private let degreeFieldOfView: CGFloat = 60
@@ -45,6 +46,8 @@ class Scene: SKScene, SKPhysicsContactDelegate {
     private let enemyDistanceFromCamera: Float = 5 //In Meters
     private let bulletRemovalDistance: Float = 20 //In Meters
     
+    private var currentScore: Int!
+    
     //Delegate Code
     public static var pendingEnemy: Bool = false
     
@@ -61,13 +64,16 @@ class Scene: SKScene, SKPhysicsContactDelegate {
         arrowLeft = foregroundNode?.childNode(withName: "arrowLeft") as? SKSpriteNode
         arrowRight = foregroundNode?.childNode(withName: "arrowRight") as? SKSpriteNode
         
+        scoreLabel = foregroundNode?.childNode(withName: "score") as? SKLabelNode
+        
+        scoreLabel.fontName = "Courier Bold"
+        
         arrowLeft?.isHidden = true
         arrowRight?.isHidden = true
         
         physicsWorld.contactDelegate = self
         
-        enemyAnchors = []
-        bulletAnchors = []
+        currentScore = 0
     }
     
     override func update(_ currentTime: TimeInterval) {
@@ -76,25 +82,27 @@ class Scene: SKScene, SKPhysicsContactDelegate {
         }
         
         // // // // // // // // // // // // // // // // // // // // // // // //
-        //  MOVE BULLETS & ENEMIES?
+        //  MOVE BULLETS & ENEMIES
         // // // // // // // // // // // // // // // // // // // // // // // //
         var bulletAnchorIndecesToRemove: [Int] = []
         
-        if (bulletAnchors!.count != 0) {
-            for bulletAnchorIndex in 0..<bulletAnchors!.count {
+        if (bulletAnchors.count > 0) {
+            for bulletAnchorIndex in 0..<bulletAnchors.count {
                 moveBullet(anchorIndex: bulletAnchorIndex)
                 
-                if (abs(bulletAnchors![bulletAnchorIndex].transform.columns.3.z) > bulletRemovalDistance) {
+                if (abs(bulletAnchors[bulletAnchorIndex].transform.columns.3.z) > bulletRemovalDistance) {
                     bulletAnchorIndecesToRemove.append(bulletAnchorIndex)
                     
-                    sceneView.session.remove(anchor: bulletAnchors![bulletAnchorIndex])
+                    sceneView.session.remove(anchor: bulletAnchors[bulletAnchorIndex])
                 }
             }
         }
         
         for i in 0 ..< bulletAnchorIndecesToRemove.count {
-            bulletAnchors!.remove(at: bulletAnchorIndecesToRemove[i] - i)
+            bulletAnchors.remove(at: bulletAnchorIndecesToRemove[i] - i)
         }
+        
+        
         
         // // // // // // // // // // // // // // // // // // // // // // // //
         //  RESPAWN ENEMIES
@@ -108,9 +116,32 @@ class Scene: SKScene, SKPhysicsContactDelegate {
         enemyRespawnCounter += 1
         
         
+        
         // // // // // // // // // // // // // // // // // // // // // // // //
-        //  ALERT CODE
+        //  ALERT CODE & SCORE
         // // // // // // // // // // // // // // // // // // // // // // // //
+        
+        leftArrowOn = false
+        rightArrowOn = false
+        
+        for enemyAnchor in enemyAnchors {
+            let enemyNode = sceneView.node(for: enemyAnchor)
+            
+            if (enemyNode?.position != nil) {
+                let contains: Bool = self.view!.frame.contains(enemyNode!.position)
+                
+                if (!contains) {
+                    leftArrowOn = true
+                    rightArrowOn = true
+                }
+            }
+        }
+        
+        arrowLeft?.isHidden = !leftArrowOn
+        arrowRight?.isHidden = !rightArrowOn
+        
+        
+        scoreLabel.text = "Score: \(currentScore!)"
     }
     
     func touchDown(atPoint pos : CGPoint) {
@@ -168,7 +199,7 @@ class Scene: SKScene, SKPhysicsContactDelegate {
             let anchor = ARAnchor(transform: transform)
             sceneView.session.add(anchor: anchor)
             
-            enemyAnchors!.append(anchor)
+            enemyAnchors.append(anchor)
         }
     }
     
@@ -189,7 +220,7 @@ class Scene: SKScene, SKPhysicsContactDelegate {
             let anchor = ARAnchor(transform: transform)
             sceneView.session.add(anchor: anchor)
             
-            bulletAnchors!.append(anchor)
+            bulletAnchors.append(anchor)
         }
         
         hasShot = true
@@ -201,30 +232,43 @@ class Scene: SKScene, SKPhysicsContactDelegate {
         }
         
         // Create anchor using the camera's current position
-        if let currentFrame = sceneView.session.currentFrame {
+        if sceneView.session.currentFrame != nil {
             var translation = matrix_identity_float4x4
             translation.columns.3.z = -1
-            let transform = simd_mul(bulletAnchors![anchorIndex].transform, translation)
+            let transform = simd_mul(bulletAnchors[anchorIndex].transform, translation)
             
-            sceneView.session.remove(anchor: bulletAnchors![anchorIndex])
+            sceneView.session.remove(anchor: bulletAnchors[anchorIndex])
             
             // Add a new anchor to the session
             let anchor = ARAnchor(transform: transform)
             sceneView.session.add(anchor: anchor)
             
-            bulletAnchors![anchorIndex] = anchor
+            bulletAnchors[anchorIndex] = anchor
         }
     }
     
     func didBegin(_ contact: SKPhysicsContact) {
-        let enemyNode = contact.bodyA.node as! SKSpriteNode
-        let bulletNode = contact.bodyB.node as! SKSpriteNode
+        guard let sceneView = self.view as? ARSKView else {
+            return
+        }
+        
+        let nodeA = contact.bodyA.node as! SKSpriteNode
+        let nodeB = contact.bodyB.node as! SKSpriteNode
         
         if ((contact.bodyA.categoryBitMask == Enemy.categoryBitMask) && (contact.bodyB.categoryBitMask == Bullet.categoryBitMask)) || (contact.bodyA.categoryBitMask == Bullet.categoryBitMask) && (contact.bodyB.categoryBitMask == Enemy.categoryBitMask) {
-            enemyNode.removeFromParent()
-            bulletNode.removeFromParent()
-        } else if(contact.bodyA.categoryBitMask == Bullet.categoryBitMask) && (contact.bodyB.categoryBitMask == Bullet.categoryBitMask) {
+            nodeA.removeFromParent()
+            nodeB.removeFromParent()
             
+            sceneView.session.remove(anchor: sceneView.anchor(for: nodeA)!)
+            sceneView.session.remove(anchor: sceneView.anchor(for: nodeB)!)
+            
+            currentScore += 50
+        } else if(contact.bodyA.categoryBitMask == Bullet.categoryBitMask) && (contact.bodyB.categoryBitMask == Bullet.categoryBitMask) {
+            nodeB.removeFromParent()
+            
+            if (sceneView.anchor(for: nodeB) != nil) {
+                sceneView.session.remove(anchor: sceneView.anchor(for: nodeB)!)
+            }
         }
     }
 }
